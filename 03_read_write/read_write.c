db7400ca12c5b9d49679d5e4d30eee651215ddb1
NamDHay <nguyennamhuy2003@gmail.com>
2024-08-27 23:11:59 +0700

    feat: Add lesson 3

--- 03_read_write/read_write.c
+++ 03_read_write/read_write.c
@@ -1,10 +1,40 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/uaccess.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("NamDHay");
 MODULE_DESCRIPTION("Register a device nr and implenment some callback function");
+
+static char buffer[255];
+static int buffer_pointer;
+
+static ssize_t driver_read(struct file *File, char *user_buffer, size_t count, loff_t *offs) {
+  int to_copy, not_copied, delta;
+
+  to_copy = min(count, buffer_pointer);
+
+  not_copied = copy_to_user(user_buffer, buffer, to_copy);
+
+  delta = to_copy - not_copied;
+  
+  return delta;
+}
+
+static ssize_t driver_write(struct file *File, const char *user_buffer, size_t count, loff_t *offs) {
+  int to_copy, not_copied, delta;
+
+  to_copy = min(count, sizeof(buffer));
+
+  not_copied = copy_to_user(buffer, user_buffer, to_copy);
+  buffer_pointer = to_copy;
+
+  delta = to_copy - not_copied;
+  
+  return delta;
+}
 
 static int driver_open(struct inode *device_file, struct file *instance){
   printk("dev_nr - open was called\n");
@@ -20,6 +50,8 @@ static struct file_operations fops = {
   .owner = THIS_MODULE,
   .open = driver_open,
   .release = driver_close
+  .read = driver_read,
+  .write = driver_write
 };
 
 #define MYMAJOR 90
